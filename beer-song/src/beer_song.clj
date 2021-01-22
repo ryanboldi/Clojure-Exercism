cf5a0a0cb7b2c8eb5ea702de4050396ba21cbe2d
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-22 20:34:05 +0400

    beer-song

---
+++ beer-song/src/beer_song.clj
@@ -0,0 +1,15 @@
+(ns beer-song)
+
+(defn verse
+  "Returns the nth verse of the song."
+  ([num] (str num " bottles of beer on the wall, " num " bottles of beer. \nTake it down and pass it around, " (- num 1) " bottles of beer on the wall."))
+  ([] (str "No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, 99 bottles of beer on the wall.")))
+
+(defn sing
+  "Given a start and an optional end, returns all verses in this interval. If
+  end is not given, the whole song from start is sung."
+  ([start] (loop [num start]
+             (if (> num 0)
+               (do (println (verse num)) (recur (- num 1)))
+               (println (verse)))))
+  ([start end] (println (str start end))))
