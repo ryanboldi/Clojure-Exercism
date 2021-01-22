8150a00b03b9c0d513c22abe66b5913dc8f82c12
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-22 20:39:00 +0400

    made some more changes to gitignore

--- beer-song/src/beer_song.clj
+++ beer-song/src/beer_song.clj
@@ -2,8 +2,8 @@
 
 (defn verse
   "Returns the nth verse of the song."
-  ([num] (str num " bottles of beer on the wall, " num " bottles of beer. \nTake it down and pass it around, " (- num 1) " bottles of beer on the wall."))
-  ([] (str "No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, 99 bottles of beer on the wall.")))
+  ([num] (str num " bottles of beer on the wall, " num " bottles of beer. \nTake it down and pass it around, " (- num 1) " bottles of beer on the wall.\n"))
+  ([] (str "No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, 99 bottles of beer on the wall.\n")))
 
 (defn sing
   "Given a start and an optional end, returns all verses in this interval. If
@@ -13,3 +13,5 @@
                (do (println (verse num)) (recur (- num 1)))
                (println (verse)))))
   ([start end] (println (str start end))))
+
+(sing 100)
