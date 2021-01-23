b02c73bb86194e8e3c679025715ad0704baf8c61
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-23 15:05:17 +0400

    row seems promising

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,7 +1,12 @@
 (ns pascals-triangle)
 
-(def triangle)
+(def triangle
+  "evaluates to pascal's triangle up to end_row"
+  [end_row])
 
-(defn row [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn row [previous]
+  "gets the next row given the previous one"
+  (loop [currentRow [1]]
+    (if (>= (clojure.core/count currentRow) (clojure.core/count previous))
+      (conj currentRow 1)
+      (recur (conj currentRow (+ (get previous (dec (clojure.core/count currentRow))) (get previous (clojure.core/count currentRow))))))))
