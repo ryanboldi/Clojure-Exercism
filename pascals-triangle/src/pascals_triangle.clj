8a4dc2cc1ff98f3ea9a373ae43a4cab561db136e
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-23 15:44:43 +0400

    passes all tests except the integer overflow one due to recursive nature of my implementation

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,20 +1,22 @@
 (ns pascals-triangle)
 
-(def triangle)
-
 (defn getRow [previous] "gets the next row given the previous one"
   (loop [currentRow [1]]
     (if (>= (clojure.core/count currentRow) (clojure.core/count previous))
       (conj currentRow 1)
       (recur (conj currentRow (+ (get previous (dec (clojure.core/count currentRow))) (get previous (clojure.core/count currentRow))))))))
 
-(defn row
+(defn getTri
   "evaluates to pascal's triangle up to end_row"
-  [end_row]
-  (loop [rows [[1]] rowNm 1]
-    (if (>= rowNm end_row)
-      (peek rows)
-      (recur (conj rows (getRow (peek rows))) (inc rowNm)))))
+  ([] (getTri 5))
+  ([end_row]
+   (loop [rows [[1]] rowNm 1]
+     (if (>= rowNm end_row)
+       rows
+       (recur (conj rows (getRow (peek rows))) (inc rowNm))))))
 
+(defn row [n]
+  (peek (getTri n)))
 
-(row 55)
+(def triangle
+  (getTri))
