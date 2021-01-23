9c4d3ed3d1b469b233ffcbac16a70b5e6603141a
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-23 15:14:34 +0400

    this works but still fails tests

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,8 +1,12 @@
 (ns pascals-triangle)
 
-(def triangle
+(defn triangle
   "evaluates to pascal's triangle up to end_row"
-  [end_row])
+  [end_row]
+  (loop [rows [[1]] rowNm 1]
+    (if (>= rowNm end_row)
+      rows
+      (recur (conj rows (row (peek rows))) (inc rowNm)))))
 
 (defn row [previous]
   "gets the next row given the previous one"
@@ -10,3 +14,5 @@
     (if (>= (clojure.core/count currentRow) (clojure.core/count previous))
       (conj currentRow 1)
       (recur (conj currentRow (+ (get previous (dec (clojure.core/count currentRow))) (get previous (clojure.core/count currentRow))))))))
+
+(triangle 55)
