7165c2124b33fc380872043ea42aa14f4c87f1e3
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-23 09:46:07 +0400

    collatz conjecture implemented with recursion

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,9 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn collatz
+  ([num steps] (if (= num 1)
+                 steps
+                 (if (clojure.core/even? num)
+                   (collatz (/ num 2) (inc steps))
+                   (collatz (inc (* 3 num)) (inc steps)))))
+  ([num] (collatz num 0)))
