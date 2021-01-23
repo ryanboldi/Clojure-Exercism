7660e62771c1eaa536737bedafa8d6bdf3084dec
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-23 15:17:13 +0400

    Update pascals_triangle.clj

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,18 +1,20 @@
 (ns pascals-triangle)
 
-(defn triangle
-  "evaluates to pascal's triangle up to end_row"
-  [end_row]
-  (loop [rows [[1]] rowNm 1]
-    (if (>= rowNm end_row)
-      rows
-      (recur (conj rows (row (peek rows))) (inc rowNm)))))
+(def triangle)
 
-(defn row [previous]
-  "gets the next row given the previous one"
+(defn getRow [previous] "gets the next row given the previous one"
   (loop [currentRow [1]]
     (if (>= (clojure.core/count currentRow) (clojure.core/count previous))
       (conj currentRow 1)
       (recur (conj currentRow (+ (get previous (dec (clojure.core/count currentRow))) (get previous (clojure.core/count currentRow))))))))
 
-(triangle 55)
+(defn row
+  "evaluates to pascal's triangle up to end_row"
+  [end_row]
+  (loop [rows [[1]] rowNm 1]
+    (if (>= rowNm end_row)
+      (peek rows)
+      (recur (conj rows (getRow (peek rows))) (inc rowNm)))))
+
+
+(row 55)
