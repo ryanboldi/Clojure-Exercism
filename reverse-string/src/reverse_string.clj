20feaa8687a3b54918ba937aab829611587d9f49
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-25 14:16:05 +0400

    Update reverse_string.clj

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,10 +1,10 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
+  ;loop through a split vector representing the string, keeping track of the reverse vector so far
   (loop [reversed [] left (clojure.string/split s #"") index 0]
     (if (>= index (clojure.core/count s))
+      ;if we reach the end, return a conjoined string of the reversed vector
       (clojure.string/join "" reversed)
-      (recur (into reversed (peek left)) (vector (drop-last left)) (inc index)))))
-
-(reverse-string "ryan")
-
+      ;if we arn't at the end, run the loop again with updated vectors and index
+      (recur (into reversed (last left)) (into [] (drop-last left)) (inc index)))))
