3b06601f27d240dd611f29f208892a73e581d5bf
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-28 08:14:35 +0400

    made reverse string significantly shorter

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,10 +1,4 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  ;loop through a split vector representing the string, keeping track of the reverse vector so far
-  (loop [reversed "" left s index 0]
-    (if (>= index (clojure.core/count s))
-      ;if we reach the end, return a conjoined string of the reversed vector
-      reversed
-      ;if we arn't at the end, run the loop again with updated vectors and index
-      (recur (str reversed (last left)) (clojure.string/join "" (drop-last left)) (inc index)))))
+  (clojure.string/join "" (into '() s)))
