1bfde2f102ae367248620b9a3e61b0630ca0a911
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-28 07:58:56 +0400

    Update collatz_conjecture.clj

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -3,7 +3,7 @@
 (defn collatz
   ([num steps] (if (= num 1)
                  steps
-                 (if (clojure.core/even? num)
+                 (if (even? num)
                    (collatz (/ num 2) (inc steps))
                    (collatz (inc (* 3 num)) (inc steps)))))
   ([num] (collatz num 0)))
