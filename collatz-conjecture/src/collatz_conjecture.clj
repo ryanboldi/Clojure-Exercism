c0c606e7ab533430a4749cc095a2878cc5edc34d
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-28 11:26:48 +0400

    Update collatz_conjecture.clj

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,12 +1,13 @@
 (ns collatz-conjecture)
 
 
-(defn doOneStep
-  [num] (if (even? num)
-          (/ num 2)
-          (inc (* 3 num))))
+(defn do-one-step [num]
+  (if (even? num)
+    (/ num 2)
+    (inc (* 3 num))))
 
-(defn collatz
-  ([num] (if (<= num 0)
-           (throw (Exception. "needs to be non-zero positive"))
-           (count (take-while (partial < 1) (iterate doOneStep num))))))
+(defn collatz [num]
+  (assert (pos? num))
+  (->> (iterate do-one-step num)
+       (take-while (partial < 1))
+       (count)))
