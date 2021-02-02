fcf556b235a3443500ab538daeb9adb96e36a625
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-02 09:48:34 +0400

    prep for cipher and get ciph letter work

--- atbash-cipher/src/atbash_cipher.clj
+++ atbash-cipher/src/atbash_cipher.clj
@@ -1,9 +1,25 @@
 (ns atbash-cipher)
 
-(defn prep_for_cipher [s]
+(defn prep_for_cipher
   "removes spaces and makes everything lowercase"
+  [s]
   (clojure.string/lower-case (clojure.string/replace (clojure.string/trim s) #"\s+" "")))
+
+(defn get_ciph_letter
+  "gets the corresponding letter"
+  [c]
+  (char (+ 96 97 (- 26 (int (.charAt c 0))))))
+
+(defn split_into_sections
+  "splits the string into sets of 4 letters"
+  [s]
+  (->>
+   (partition 4 2 "" s)
+   (map (partial clojure.string/join ""))
+   (clojure.string/join " ")))
 
 (defn encode [] ;; <- arglist goes here
   ;; your code goes here
   )
+
+(get_ciph_letter "z")
