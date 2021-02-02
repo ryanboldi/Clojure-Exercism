8df66c975b035c5a8ce5517ed6663677a642f313
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-02 09:34:29 +0400

    prep for cipher function works

--- atbash-cipher/src/atbash_cipher.clj
+++ atbash-cipher/src/atbash_cipher.clj
@@ -1,5 +1,9 @@
 (ns atbash-cipher)
 
+(defn prep_for_cipher [s]
+  "removes spaces and makes everything lowercase"
+  (clojure.string/lower-case (clojure.string/replace (clojure.string/trim s) #"\s+" "")))
+
 (defn encode [] ;; <- arglist goes here
   ;; your code goes here
-)
+  )
