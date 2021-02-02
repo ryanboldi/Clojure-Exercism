06fe39873149f2c89ab926ad27a74e43fba2d706
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-02 08:26:19 +0400

    much more beautiful

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -7,15 +7,12 @@
     (and (every? #(Character/isUpperCase %) letters) (seq letters))))
 
 (defn get-response [s]
-  (if (empty? s)
-    "Fine. Be that way!"
-    (if (question? s)
-      (if (yelling? s)
-        "Calm down, I know what I'm doing!"
-        "Sure.")
-      (if (yelling? s)
-        "Whoa, chill out!"
-        "Whatever."))))
+  (cond
+    (empty? s) "Fine. Be that way!"
+    (and (question? s) (yelling? s)) "Calm down, I know what I'm doing!"
+    (question? s) "Sure."
+    (yelling? s) "Whoa, chill out!"
+    :else "Whatever."))
 
 (defn response-for [s]
   (get-response (clojure.string/trim-newline (clojure.string/trim s))))
