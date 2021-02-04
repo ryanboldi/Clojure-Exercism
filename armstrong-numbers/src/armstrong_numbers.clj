1e44fb588fb94fdd7fb9b2645ad3b1c204f1fd58
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-04 08:21:17 +0400

    replaced with library function

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,4 +1,5 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  (:require [clojure.math.numeric-tower :as math]))
 
 (defn get_int_vec [num] (into [] (map #(Character/digit % 10) (into [] (str num)))))
 
@@ -6,6 +7,6 @@
   (loop [number (get_int_vec num) length (count number) index 0  sum 0]
     (if (>= index length)
       (= (int sum) num)
-      (recur (butlast number) length (inc index) (+ sum (Math/pow (last number) length))))))
+      (recur (butlast number) length (inc index) (+ sum (math/expt (last number) length))))))
 
 (armstrong? 9)
