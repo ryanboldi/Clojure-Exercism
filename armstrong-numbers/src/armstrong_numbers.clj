b17c224f9831db588216b55166827b5c93f95b02
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-04 08:24:15 +0400

    handles big numbers: seems to work well now

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -4,9 +4,7 @@
 (defn get_int_vec [num] (into [] (map #(Character/digit % 10) (into [] (str num)))))
 
 (defn armstrong? [num]
-  (loop [number (get_int_vec num) length (count number) index 0  sum 0]
+  (loop [number (get_int_vec num) length (count number) index 0 sum 0]
     (if (>= index length)
-      (= (int sum) num)
+      (= (bigint sum) num)
       (recur (butlast number) length (inc index) (+ sum (math/expt (last number) length))))))
-
-(armstrong? 9)
