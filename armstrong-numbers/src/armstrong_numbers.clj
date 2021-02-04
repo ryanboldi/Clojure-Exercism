f63bdebf1b949d6115925389c5237fb6a435d637
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-04 08:12:20 +0400

    started work on determining whether or not number is an armstrong number

---
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -0,0 +1,11 @@
+(ns armstrong-numbers)
+
+(defn get_int_vec [num] (into [] (map #(Character/digit % 10) (into [] (str num)))))
+
+(defn armstrong? [num]
+  (loop [number (get_int_vec num) length (count number) index 0  sum 0]
+    (if (>= index length)
+      (do (println sum) (= sum num))
+      (recur (butlast number) length (inc index) (+ sum (Math/pow (last number) length))))))
+
+(armstrong? 9)
