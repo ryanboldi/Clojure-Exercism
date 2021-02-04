0581fce93403aa0d99fdd8d6da1989eb13662c0e
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-04 08:13:43 +0400

    seems to work

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -5,7 +5,7 @@
 (defn armstrong? [num]
   (loop [number (get_int_vec num) length (count number) index 0  sum 0]
     (if (>= index length)
-      (do (println sum) (= sum num))
+      (= (int sum) num)
       (recur (butlast number) length (inc index) (+ sum (Math/pow (last number) length))))))
 
 (armstrong? 9)
