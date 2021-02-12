85a2045640f50e89951a90a107b5124caa79f420
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-12 17:10:18 +0400

    Update armstrong_numbers.clj

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -5,8 +5,4 @@
   (map #(mod % 10) (take-while #(> % 0) (iterate #(quot % 10) num))))
 
 (defn armstrong? [num]
-  (loop [number (get-int-vec num) length (count number) index 0 sum 0]
-    (if (>= index length)
-      (= (bigint sum) num)
-      (recur (butlast number) length (inc index) (+ sum (math/expt (last number) length))))))
-
+  (= num (reduce + (map #(math/expt % (count (str num))) (get-int-vec num)))))
