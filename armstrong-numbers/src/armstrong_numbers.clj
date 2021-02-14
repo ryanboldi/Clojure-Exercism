fc93ef7696488121d29cafee57cbc882ca032ebc
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-14 12:27:58 +0400

    Update armstrong_numbers.clj

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,8 +1,14 @@
-(ns armstrong-numbers
-  (:require [clojure.math.numeric-tower :as math]))
+(ns armstrong-numbers)
 
 (defn get-int-vec [num]
-  (map #(mod % 10) (take-while #(> % 0) (iterate #(quot % 10) num))))
+  (->> num
+       (iterate #(quot % 10))
+       (take-while #(>  % 0))
+       (map #(mod % 10))))
+
+(defn pow
+  [base exp]
+  (apply * (repeat exp base)))
 
 (defn armstrong? [num]
-  (= num (reduce + (map #(math/expt % (count (str num))) (get-int-vec num)))))
+  (= num (apply + (map #(pow % (count (str num))) (get-int-vec num)))))
