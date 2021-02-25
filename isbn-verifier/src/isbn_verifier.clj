6c955759c2b84c285e0588b081202ee6558fa647
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-25 10:54:53 +0400

    Update isbn_verifier.clj

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -4,19 +4,22 @@
   [number-list]
   (reduce +
           (map (fn [base pow] (reduce * (repeat pow base)))
-               (map #(Character/digit % 10) number-list) 
+               (map #(Character/digit % 10) number-list)
                (reverse (range 11)))))
+
+(sum-with-powers '(1 2 3 4 5 6 7 8 9))
 
 (defn isbn? [isbn]
   (->> isbn
        (remove #{\-})
-       (butlast)
-       (sum-with-powers)
-       (+ (if (= \X (last isbn))
-            10
-            (last isbn)))
-       #(mod % 11)
-       (= 0)))
+       (count)
+       ;(butlast)
+       ;(sum-with-powers)
+       ;(+ (if (= \X (last isbn))
+      ;    10
+      ;      (Character/digit (last isbn) 10)))
+      ; (#(mod % 11))
+       ))
 
 (isbn? "3-598-31508-8")
 (butlast "ryan")
