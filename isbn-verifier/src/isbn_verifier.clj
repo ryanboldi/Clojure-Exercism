54fce1ae005180f60c80876ea83b399673b4a883
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-25 09:14:44 +0400

    Update isbn_verifier.clj

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -3,3 +3,19 @@
 (defn isbn? [isbn] ;; <- arglist goes here
   ;; your code goes here
 )
+
+(defn get-int-list
+  [number]
+  (->> number
+       (iterate #(quot % 10))
+       (take-while #(> % 0))
+       (map #(mod % 10))
+       reverse))
+
+(defn sum-with-powers
+  [number-list]
+  ())
+
+(range 11)
+(zipmap '(3 5 9 8 2 1 5 0 8 8) (reverse (range 10)))
+
