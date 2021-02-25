873ac66f9f78925d696906f7b697856c8a99b326
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-25 12:15:54 +0400

    works

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,26 +1,23 @@
 (ns isbn-verifier)
 
-(defn sum-with-powers
+(defn sum-with-mults
   [number-list]
   (reduce +
-          (map (fn [base pow] (reduce * (repeat pow base)))
+          (map (fn [base mult] (* base mult))
                (map #(Character/digit % 10) number-list)
                (reverse (range 11)))))
-
-(sum-with-powers '(1 2 3 4 5 6 7 8 9))
 
 (defn isbn? [isbn]
   (->> isbn
        (remove #{\-})
-       (count)
-       ;(butlast)
-       ;(sum-with-powers)
-       ;(+ (if (= \X (last isbn))
-      ;    10
-      ;      (Character/digit (last isbn) 10)))
-      ; (#(mod % 11))
-       ))
-
-(isbn? "3-598-31508-8")
-(butlast "ryan")
-
+       (butlast)
+       (remove #(re-matches #"[A-Za-z]" (str %)))
+       (sum-with-mults)
+       (+ (if (= \X (last isbn))
+            10
+            (Character/digit (last isbn) 10)))
+       (#(mod % 11))
+       (= 0)
+       (and (re-matches #"\d|[X]" (str (last isbn))))
+       (and (= 10 (count (remove #{\-} isbn))))
+       boolean))
