af4e4a305dd977dde5b9d65479456c69630e4afa
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-28 12:12:21 +0400

    Update isbn_verifier.clj

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,4 +1,5 @@
-(ns isbn-verifier)
+(ns isbn-verifier
+  (:require [clojure.string :as str]))
 
 (defn sum-with-mults
   [number-list]
@@ -9,15 +10,14 @@
 
 (defn isbn? [isbn]
   (->> isbn
-       (remove #{\-})
+       (#(str/replace % #"-" ""))
        (butlast)
-       (remove #(re-matches #"[A-Za-z]" (str %)))
        (sum-with-mults)
        (+ (if (= \X (last isbn))
             10
             (Character/digit (last isbn) 10)))
        (#(mod % 11))
-       (= 0)
-       (and (re-matches #"\d|[X]" (str (last isbn))))
-       (and (= 10 (count (remove #{\-} isbn))))
-       boolean))
+       zero?
+       (and (re-matches #"^(\d{9}(\d|[X]))$" (str/replace isbn "-" "")))
+       (#(or (= false %) (nil? %)))
+       not))
