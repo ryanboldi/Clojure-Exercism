7ce202a4f0398948b1fd94c9f14135b1738ff507
Ryan Boldi <ryan.boldi123@gmail.com>
2021-03-05 13:07:03 +0400

    seems to work

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -1,5 +1,15 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string]))
+
+(defn count-words
+  [[frst & rst :as part]]
+  (hash-map frst (count part)))
 
 (defn word-count [s] ;; <- arglist goes here
   ;; your code goes here
-)
+  (->> s
+       (re-seq #"\w+")
+       (map clojure.string/lower-case)
+       (partition-by identity)
+       (map count-words)
+       (into {})))
