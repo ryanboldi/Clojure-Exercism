04125f55ca042485928a5c000634b8ee26f11b71
Ryan Boldi <ryan.boldi123@gmail.com>
2021-03-10 23:16:16 +0400

    finished first iteration of anagram

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,5 +1,12 @@
 (ns anagram)
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn anagram?
+  [template prospect]
+  (and
+   (= (sort template) (sort prospect))
+   (not= template prospect)))
+
+(defn anagrams-for [word prospect-list]
+  (filter
+   #(anagram? (clojure.string/lower-case word)
+              (clojure.string/lower-case %)) prospect-list))
